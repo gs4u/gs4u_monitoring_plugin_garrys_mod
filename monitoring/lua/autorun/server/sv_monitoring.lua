b695505b76841d63cd1411d00a08f7b75e7e2bd6
Rustgame <52970661+RustgameSteam@users.noreply.github.com>
2020-07-05 00:45:48 +0500

    Обновление Аддона
    Добавлен авто перезапуск плагина, при ошибках вызванным DarkRP Ulx и прочее.

--- monitoring/lua/autorun/server/sv_monitoring.lua
+++ monitoring/lua/autorun/server/sv_monitoring.lua
@@ -4,148 +4,156 @@ monversion = "3.6.9.3"
 
 --______________________________________________________________________________________________
 --
---							[	Настройка аддона для мониторинга	]							
+--			[	Настройка аддона для мониторинга	]							
 --
 --‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 
-local APIUrl					= 	"https://api.gs4u.net/"
-local IDServer 					=	"" -- Server ID from GS4u.net
-local WeaponsAddons				= 	"M9K" --M9K, SWBase, FAS...
-local ServerToken				=	"" --Server Token from GS4u.net
-local LangIndex_Monitoring 		= 	"ru"
+local APIUrl					= 	"https://api.gs4u.net/" -- API Сайт
+local IDServer 					=	""-- ID Сервера
+local WeaponsAddons				= 	""	-- M9K, SWBase, FAS и тд
+local ServerToken				=	"" --Токен Сервера
+local LangIndex_Monitoring 			= 	"ru" 	-- Язык
+local MoneysName				=	"₽" 	-- Валюта - Для DarkRP
 
 --[[
-	Языки/Langsuages
+	Языки/Langs
 	en - English
 	ru - Русский
 	de - Deutsch
 ]]
 
-MoneysName				=	"₽" -- Currency - for DarkRP
+print("[ GS4u Monitoring ] Запуск аддона!")
+local function StartMonitoring()
+	timer.Create("RefreshTimer_Timer", 1, 1, function()
+		print("[ GS4u ] GetCoolDown")
+		http.Fetch(APIUrl.. "?cmd=getCoolDown&data={\"cmd\":\"updateServer\"}", function(CallBack)
+			print("[ GS4u ] TakeCoolDown: "..tonumber(CallBack).."s")
+			timer.Create("RefreshTimer_Timer", tonumber(CallBack), 0, function()
+				local PlayerList 	= ""
+				local CommandsList 	= ""
+				local CategoryJobs 	= ""
 
-print("[ GS4u ] Starting Addons - Monitoring")
-timer.Create("RefreshTimer_Timer", 1, 1, function()
-	print("[ GS4u ] GetCoolDown")
-	http.Fetch(APIUrl.. "?cmd=getCoolDown&data={\"cmd\":\"updateServer\"}", function(CallBack)
-		print("[ GS4u ] TakeCoolDown: "..tonumber(CallBack).."s")
-		timer.Create("RefreshTimer_Timer", tonumber(CallBack), 0, function()
-			local PlayerList 	= ""
-			local CommandsList 	= ""
-			local CategoryJobs 	= ""
+				local OSList 		= ""
+				local iPassword		= ""
+				if (system.IsLinux()) then
+					OSList = "Linux"
+				elseif (system.IsWindows()) then
+					OSList = "Windows"
+				elseif (system.IsOSX()) then
+					OSList = "OSX"
+				end
 
-			local OSList 		= ""
-			local iPassword		= ""
-			if (system.IsLinux()) then
-				OSList = "Linux"
-			elseif (system.IsWindows()) then
-				OSList = "Windows"
-			elseif (system.IsOSX()) then
-				OSList = "OSX"
-			end
+				iBotsCount = 0
+				for k, v in pairs(player.GetBots()) do
+					iBotsCount = iBotsCount + 1
+				end
 
-			iBotsCount = 0
-			for k, v in pairs(player.GetBots()) do
-				iBotsCount = iBotsCount + 1
-			end
-
-			if (GetConVarString("sv_password") == "") then
-				iPassword = "0"
-			else
-				iPassword = "1"
-			end
-
-			CommandsList = "{\"sv_cheats\":\"" 	..GetConVarNumber("sv_cheats").. "\",\
-							\"tickrate\":\"" 	..math.floor(FrameTime()).. "\",\
-							\"uptime\":\"" 		..CurTime().. "\",\
-							\"appid\":\"4000\",\
-							\"gamename\":\"Garry\'s Mod\",\
-							\"game\":\"garrysmod\",\
-							\"countbots\":" 	..iBotsCount.. ",\
-							\"password\":" 		..iPassword.. ",\
-							\"os\":\"" 			..OSList.. "\",\
-							\"plversion\":\"" 	..monversion.. "\"}"
-			-- 				[ Краш тест ]			--
-			--[[for i = 1, 900, 1 do
-				PlayerList = string.format("{\"name\":\"TEASASDASD" ..i.. "\",\"kills\":\"" ..i.. "\",\"deaths\":\"" ..i.. "\",\"ping\":\"BOTCrash\"}," ..PlayerList.. "")
-			end]]
-			--										--
-			for k,v in pairs(RPExtraTeams) do
-				szdescription = string.Replace(v.description, "\t", "")
-				szdescription = string.Replace(szdescription, "\n", "[br]")
-				szdescription = string.Replace(szdescription, "\"", "")
-				szdescription = string.Replace(szdescription, "\\", "")
-
-				CategoryJobs = ("{\"name\":\"" ..v.category.. "\",\"data\":[{\"name\":\"" ..v.name.. "\",\"description\":\"" ..szdescription.. "\",\"salary\":\"" ..v.salary.. " " ..MoneysName.. "\"}]}," ..CategoryJobs.. "")
-			end
-
-			CategoryJobs = "[" ..CategoryJobs.. "]"
-			CategoryJobs = string.Replace(CategoryJobs, ",]", "]")
-
-			--print(CategoryJobs) 
-
-			for k, v in pairs(player.GetAll()) do
-				Ping = 0
-				if (v:Ping() <= 1) then
-					Ping = "BOT"
+				if (GetConVarString("sv_password") == "") then
+					iPassword = "0"
 				else
-					Ping = v:Ping()
+					iPassword = "1"
 				end
 
-				PlayerList = string.format("{\"ulxadmins\":\"" ..v:GetUserGroup().. "\",\
-							\"name\":\"" 	..v:Nick().. "\",\
-							\"kills\":" 	..v:Frags().. ",\
-							\"deaths\":" 	..v:Deaths().. ",\
-							\"ping\":\"" 	..Ping.. "\",\
-							\"job\":\"" 	..v:getDarkRPVar("job").. "\",\
-							\"moneys\":\"" 	..v:getDarkRPVar("money").. "\"}," ..PlayerList.. "")
-			end
+				CommandsList = "{\"sv_cheats\":\"" 	..GetConVarNumber("sv_cheats").. "\",\
+								\"tickrate\":\"" 	..math.floor(FrameTime()).. "\",\
+								\"uptime\":\"" 		..CurTime().. "\",\
+								\"appid\":\"4000\",\
+								\"gamename\":\"Garry\'s Mod\",\
+								\"game\":\"garrysmod\",\
+								\"countbots\":" 	..iBotsCount.. ",\
+								\"password\":" 		..iPassword.. ",\
+								\"os\":\"" 			..OSList.. "\",\
+								\"plversion\":\"" 	..monversion.. "\"}"
+				-- 				[ Краш тест ]			--
+				--[[for i = 1, 900, 1 do
+					PlayerList = string.format("{\"name\":\"TEASASDASD" ..i.. "\",\"kills\":\"" ..i.. "\",\"deaths\":\"" ..i.. "\",\"ping\":\"BOTCrash\"}," ..PlayerList.. "")
+				end]]
+				--										--
+				for k,v in pairs(RPExtraTeams) do
+					szdescription = string.Replace(v.description, "\t", "")
+					szdescription = string.Replace(szdescription, "\n", "[br]")
+					szdescription = string.Replace(szdescription, "\"", "")
+					szdescription = string.Replace(szdescription, "\\", "")
 
-			PlayerList = "[" ..PlayerList.. "]"
-			PlayerList = string.Replace(PlayerList, ",]", "]")
+					CategoryJobs = ("{\"name\":\"" ..v.category.. "\",\"data\":[{\"name\":\"" ..v.name.. "\",\"description\":\"" ..szdescription.. "\",\"salary\":\"" ..v.salary.. " " ..MoneysName.. "\"}]}," ..CategoryJobs.. "")
+				end
 
-			PostUrl = "{\"n\":\"" 	..GetHostName().. "\",\
-			\"id\":\"" 				..IDServer.. "\",\
-			\"t\":\"sdk2013\",\
-			\"m\":\"" 				..game.GetMap().. "\",\
-			\"gamemode\":\"" 		..engine.ActiveGamemode().. "\",\
-			\"pc\":\"" 				..player.GetCount().. "\",\
-			\"pm\":\"" 				..game.MaxPlayers().. "\",\
-			\"e\":" 				..CommandsList.. ",\
-			\"p\":" 				..PlayerList.. ",\
-				\"tabs\":[{\"name\":\"jobs\",\"data\":"..CategoryJobs.. "}]}"
+				CategoryJobs = "[" ..CategoryJobs.. "]"
+				CategoryJobs = string.Replace(CategoryJobs, ",]", "]")
 
-			HashCode 	= sha1(PostUrl .. ServerToken)
-			--PostUrl 	= string.Replace(PostUrl, " ", "%20")
+				--print(CategoryJobs) 
 
-			http.Post("" ..APIUrl.. "index.php", { cmd = "updateServer", data = PostUrl, s = HashCode, hash = "sha1" }, 
-			function(CallBack_Information)
-				print(CallBack_Information)
-				if (CallBack_Information ~= "0" or CallBack_Information == "") then
-					if (LangIndex_Monitoring == "ru") then
-						print("Monitoring | " .. "Ошибка: Код " ..CallBack_Information)
-						print("Monitoring | " .. "Прочтите документацию аддона на сайте.")
-					elseif (LangIndex_Monitoring == "de") then
-						print("Monitoring | " .. "Fehler: Code " ..CallBack_Information)
-						print("Monitoring | " .. "Lesen Sie die Dokumentation des Plugins.")
+				for k, v in pairs(player.GetAll()) do
+					Ping = 0
+					if (v:Ping() <= 1) then
+						Ping = "BOT"
 					else
-						print("Monitoring | " .. "Error: Code " ..CallBack_Information)
-						print("Monitoring | " .. "Read the addon documentation on the site.")
+						Ping = v:Ping()
 					end
+
+					PlayerList = string.format("{\"ulxadmins\":\"" ..v:GetUserGroup().. "\",\
+								\"name\":\"" 	..v:Nick().. "\",\
+								\"kills\":" 	..v:Frags().. ",\
+								\"deaths\":" 	..v:Deaths().. ",\
+								\"ping\":\"" 	..Ping.. "\",\
+								\"job\":\"" 	..v:getDarkRPVar("job").. "\",\
+								\"moneys\":\"" 	..v:getDarkRPVar("money").. "\"}," ..PlayerList.. "")
 				end
+
+				PlayerList = "[" ..PlayerList.. "]"
+				PlayerList = string.Replace(PlayerList, ",]", "]")
+
+				PostUrl = "{\"n\":\"" 	..GetHostName().. "\",\
+				\"id\":\"" 				..IDServer.. "\",\
+				\"t\":\"sdk2013\",\
+				\"m\":\"" 				..game.GetMap().. "\",\
+				\"gamemode\":\"" 		..engine.ActiveGamemode().. "\",\
+				\"pc\":\"" 				..player.GetCount().. "\",\
+				\"pm\":\"" 				..game.MaxPlayers().. "\",\
+				\"e\":" 				..CommandsList.. ",\
+				\"p\":" 				..PlayerList.. ",\
+					\"tabs\":[{\"name\":\"jobs\",\"data\":"..CategoryJobs.. "}]}"
+
+				HashCode 	= sha1(PostUrl .. ServerToken)
+				--PostUrl 	= string.Replace(PostUrl, " ", "%20")
+
+				http.Post("" ..APIUrl.. "index.php", { cmd = "updateServer", data = PostUrl, s = HashCode, hash = "sha1" }, 
+				function(CallBack_Information)
+					if (CallBack_Information ~= "0" or CallBack_Information == "") then
+						if (LangIndex_Monitoring == "ru") then
+							print("Monitoring | " .. "Ошибка: Код " ..CallBack_Information)
+							print("Monitoring | " .. "Прочтите документацию аддона на сайте.")
+						elseif (LangIndex_Monitoring == "de") then
+							print("Monitoring | " .. "Fehler: Code " ..CallBack_Information)
+							print("Monitoring | " .. "Lesen Sie die Dokumentation des Plugins.")
+						else
+							print("Monitoring | " .. "Error: Code " ..CallBack_Information)
+							print("Monitoring | " .. "Read the addon documentation on the site.")
+						end
+					end
+				end,
+				function(CallBack_ErrorMessage)
+					print(CallBack_ErrorMessage)
+				end)
+				CategoryJobs= ""
+				PlayerList 	= ""
+				PostUrl 	= ""
+				HashCode 	= ""
 			end,
-			function(CallBack_ErrorMessage)
-				print(CallBack_ErrorMessage)
+				function(CallBack_Error)
+					print(CallBack_Error);
 			end)
-			CategoryJobs= ""
-			PlayerList 	= ""
-			PostUrl 	= ""
-			HashCode 	= ""
 		end,
-			function(CallBack_Error)
-				print(CallBack_Error);
+		function(errors)
+			print(errors)
 		end)
-	end,
-	function(errors)
-		print(errors)
 	end)
+end
+
+StartMonitoring()
+
+timer.Create("ReloadAddons", 1800, 0, function()
+	StartMonitoring()
+	print("[ GS4u Monitoring ] Перезапуск аддона прошел успешно!")
 end)
+print("[ GS4u Monitoring ] Запуск аддона прошел успешно!")
